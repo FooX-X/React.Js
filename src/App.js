813eec069582ba6dae438de872ae23e76660c8c5
FooX-X <40070777+FooX-X@users.noreply.github.com>
2018-11-20 19:23:13 +0200

    nd3: create form

--- src/App.js
+++ src/App.js
@@ -2,35 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 class App extends Component {
     constructor () {
-      super();
-      this.state = {value1: '1das',
-                    value2: '2sad',
-                    value3: '3dasd',
-                  }
+        super();
+        this.state = {
+            value1: '1das',
+            value2: '2sad',
+            value3: '3dasd',
+        }
     }
 
-    handleChange1 (event, value) {
-      this.setState({'value1': event.target.value})
-    }
-    handleChange2 (event, value) {
-      this.setState({'value2': event.target.value})
-    }
-    handleChange3 (event, value) {
-      this.setState({'value3': event.target.value})
+    handleChange (event, value) {
+        let paramsToSet = {}
+        paramsToSet[value] = event.target.value
+        this.setState(paramsToSet)
     }
 
     render() {
         return (
             <div className="App">
-                <input onChange={this.handleChange1.bind(this)} value={this.state.value1} />
+                <input onChange={(event)=>this.handleChange(event, 'value1')} value={this.state.value1} />
                 <div>
                     {this.state.value1}
                 </div>
-                <input onChange={this.handleChange2.bind(this)} value={this.state.value2} />
+                <input onChange={(event)=>this.handleChange(event, 'value2')} value={this.state.value2} />
                 <div>
                     {this.state.value2}
                 </div>
-                <input onChange={this.handleChange3.bind(this)} value={this.state.value3} />
+                <input onChange={(event)=>this.handleChange(event, 'value3')} value={this.state.value3} />
                 <div>
                     {this.state.value3}
                 </div>
@@ -44,3 +41,4 @@ class App extends Component {
 }
 
 export default App;
+
